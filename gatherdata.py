95039cf6242b90327e4a7406b7b37ca1e92d925f
Philip Roche <phil@tinyviking.ie>
2016-04-13 10:17:46 +0100

    Initial commit

---
+++ gatherdata.py
@@ -0,0 +1,20 @@
+import random
+import decimal
+from helpers import prompt
+from pndatapush.offline import Offline
+from pndatapush.pushdata import PNPushData
+
+def gen_random_decimal(i, d):
+	return decimal.Decimal('%d.%d' % (random.randint(0,i),random.randint(0,d)))
+
+
+def wait_for_data():
+	offline = Offline(payload_consumers=[PNPushData])
+	while True:
+		sensor_payload = prompt('Enter Sensor Data', default=gen_random_decimal(35,99))
+		print('Pushing %s to your AEP of choice ' % str(sensor_payload))
+		offline.save('123456',sensor_payload)
+
+if __name__ == '__main__':
+	print('runs as a script,',__name__)
+	wait_for_data()
