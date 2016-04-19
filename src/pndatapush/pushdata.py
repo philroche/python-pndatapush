9475f8a6939abf5c8bb8c26f68ec547aa14a7baa
Philip Roche <phil@tinyviking.ie>
2016-04-19 16:18:57 +0100

    Auth token for Pervasive Nation data push now reads from environment variable PERVASIVENATION_AUTHTOKEN

--- src/pndatapush/pushdata.py
+++ src/pndatapush/pushdata.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import os
 from abc import ABCMeta, abstractmethod
 
 
@@ -25,13 +25,18 @@ class PNPushData(PushDataBase):
     def push(self, sensordata):
         print('Pushing [%d] %s data to PN %s with timestamp %s' % (sensordata.id, str(sensordata.deviceid), str(sensordata.payload), str(sensordata.timestamp)))
         payload = {"payload": str(sensordata.payload)}
-        headers = {"Authorization": "Bearer MYREALLYLONGTOKENIGOT"}
+
+        headers = {"Authorization": "Bearer %s" % os.environ.get('PERVASIVENATION_AUTHTOKEN', None)}
         #url = 'https://api.pervasivenation.com'
         url = 'http://127.0.0.1:8000/publish'
         try:
             r = requests.post(url, data=json.dumps(payload), headers=headers)
             if r.status_code == 200:
                 return True
+            elif r.status_code == 401:
+                print('Authorization token is incorrect or not set. '
+                      'Use environment variable PERVASIVENATION_AUTHTOKEN to set token.')
+                return False
         except requests.ConnectionError as conn_error:
-            pass
+            print('There was a connection error connecting to Pervasive Nation.')
         return False
